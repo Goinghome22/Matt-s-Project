a6bc721e7006e9c6931b9174f95ef7a8e4ce52f6
Adult <Adult@DAEDMAC02.local>
2025-09-24 15:10:14 -0400

    Did a refresh of the stack activity from a while back and pushed it to Github

---
+++ 6 month/coding_practice/stack_practice.py
@@ -0,0 +1,35 @@
+stack = []
+
+def push(item):
+    stack.append(item)
+    
+def pop():
+    if not is_empty():
+        return stack.pop()
+    else:
+        return "Stack is empty!"
+    
+def peek():
+        if not is_empty():
+            return stack[-1]
+        else:
+            return "Stack is empty!"
+        
+def is_empty():
+            return len(stack) == 0
+        
+def size():
+    return len(stack)
+
+push(6)
+push(7)
+push(10)
+print("Top of stack:", peek())
+print("Stack size", size())
+print("Popped:", pop())
+print("Popped:", pop())
+print("Is stack empty?", is_empty())
+print("Popped:", pop())
+print("Popped:", pop())
+
+            
